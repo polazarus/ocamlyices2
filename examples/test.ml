29ec82d661ea7bd8b1cd160756971298d0d1354f
Mickaël Delahaye <mickael.delahaye@gmail.com>
2014-09-20 11:10:28 +0200

    Fix so-called example test.ml

--- examples/test.ml
+++ examples/test.ml
@@ -7,55 +7,46 @@ let () =
   print_endline build_date
 
 let () =
-  Yices2.init ();
+  let _ = Term.true_ () in
+  let _ = Term.false_ () in
+  let _ = Term.Arith.of_int 1 in
+  let _ = Term.Arith.of_int32 32l in
+  let i64 = Term.Arith.of_int64 64L in
+  let bv1 = Term.Bitvector.zero 5 in
+  assert (Term.is_bitvector bv1);
+  assert (not (Term.is_bitvector i64));
 
-  let _ = true_ () in
-  let _ = false_ () in
-  let _ = int_ 1 in
-  let _ = int32_ 32l in
-  let i64 = int64_ 64L in
-  let bv1 = bvconst_zero 5 in
-  assert (term_is_bitvector bv1);
-  assert (Pervasives.not (term_is_bitvector i64));
-  
-  assert (5 = (term_bitsize bv1));
+  assert (5 = (Term.bitsize bv1));
   let () =
     try
-      let _ = term_bitsize i64 in
+      let _ = Term.bitsize i64 in
       assert false
-    with YicesError (msg, r) ->
-      print_endline msg; print_int r.code;
-      assert (r.code == 24)
+    with YicesError (code, r) ->
+      print_endline r.Error.name;
+      assert (code == Error.BITVECTOR_REQUIRED)
   in 
-  
-  let bool_typ = bool_type () in
-  let var1 = new_uninterpreted_term bool_typ in
-  let var2 = new_uninterpreted_term bool_typ in
-  let formula = and2 (implies var1 var2) var1 in
-  let cfg = new_config () in
-  let ctx = new_context cfg in
-  assert_formula ctx formula;
-  let status = check ctx in
-  let model = get_model ctx in
-  Printf.printf "v1 %B,  v2 %B\n" (get_bool_value model var1) (get_bool_value model var2);
-  free_model model;
-  (* let _ = get_bool_value model var1 in*)
-  free_context ctx;
-  
+
+  let bool_typ = Type.bool () in
+  let var1 = Term.new_uninterpreted bool_typ in
+  let var2 = Term.new_uninterpreted bool_typ in
+  let formula = Term.and2 (Term.implies var1 var2) var1 in
+  begin
+    let ctx = Context.create () in
+    Context.assert_formula ctx formula;
+    let status = Context.check ctx in
+    let model = Context.get_model ctx in
+    Printf.printf "v1 %B,  v2 %B\n" (Context.Model.get_bool model var1) (Context.Model.get_bool model var2);
+    (* let _ = get_bool_value model var1 in*)
+  end;
+
   print_endline "**";
-  
-  let ctx = new_context cfg in
-  let int_typ = int_type () in
-  let var = new_uninterpreted_term int_typ in
-  let formula = arith_eq var (power (int_ 2) 62) in
-  assert_formula ctx formula;
-  let status = check ctx in
-  let model = get_model ctx in
-  Printf.printf "v %d\n" (get_int_value model var);
-  (* free_model model;
-  free_context ctx; *)
-  
-  let v1 = Yices2.int_ 60 in
-  let v2 = Yices2.int_ 62 in
-  let _ = Yices2.add v1 v2 in
-  Yices2.exit ()
+
+  let ctx = Context.create () in
+  let int_typ = Type.int () in
+  let var = Term.new_uninterpreted int_typ in
+  let formula = Term.Arith.eq var (Term.Arith.power (Term.Arith.of_int 2) 62) in
+  Context.assert_formula ctx formula;
+  let status = Context.check ctx in
+  let model = Context.get_model ctx in
+  Printf.printf "v %Ld\n" (Context.Model.get_int64 model var);
+  Printf.printf "v %a\n" Z.output (Context.Model.get_z model var);
