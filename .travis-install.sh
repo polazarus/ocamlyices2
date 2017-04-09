0c50ebc98eec21ca18b4de1e2a36fc3252062009
Maël Valais <mael.valais@gmail.com>
2017-04-09 22:15:40 +0200

    travis-ci: switched from opam11 to opam12
    Travis CI was complaining that opam cannot update itself
    as it is too old; I just picked a more recent version of opam
    using a different ppa.

--- .travis-install.sh
+++ .travis-install.sh
@@ -6,17 +6,18 @@ DEBIAN_PACKAGES="$DEBIAN_PACKAGES gperf"
 fi
 
 case "$OCAML_VERSION" in
-3|3.12|3.12.1) ppa=avsm/ocaml312+opam11 ;;
-4.00|4.00.1) ppa=avsm/ocaml40+opam11 ;;
-4.01|4.01.0) ppa=avsm/ocaml41+opam11 ;;
-4|4.02|4.02.0) ppa=avsm/ocaml42+opam11 ;;
+3|3.12|3.12.1) ppa=avsm/ocaml312+opam12 ;;
+4.00|4.00.1) ppa=avsm/ocaml40+opam12 ;;
+4.01|4.01.0) ppa=avsm/ocaml41+opam12 ;;
+4|4.02|4.02.0) ppa=avsm/ocaml42+opam12 ;;
 *) echo Unknown OCaml version: $OCAML_VERSION; exit 1 ;;
 esac
 
 sudo add-apt-repository -y ppa:$ppa
 sudo apt-get update -qq
+# if NOGPERF is not defined, ${NOGPERF:+gperf} will expand to nothing
 sudo apt-get install -qq $DEBIAN_PACKAGES
 
 opam init -y
 eval `opam config env`
-opam install -y ocamlfind zarith
+opam install -y ocamlfind zarith ounit
