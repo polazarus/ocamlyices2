e326d8f68cf00bb4541f26ebfdc7074098f3e76e
Maël Valais <mael.valais@gmail.com>
2017-04-22 17:23:24 +0200

    ocamlyices2: checkoverflow test: fixed the the 'is_long_int' test on 32bit arch
    * also use 'Sys.word_size' instead of 'max_int'
    * also written an error message to help debugging when the test fails.

--- tests/checkoverflow.ml
+++ tests/checkoverflow.ml
@@ -2,7 +2,7 @@ open Yices2
 open OUnit2
 
 let checkoverflow _ =
-  let has_long_in = (max_int lsl 34) != 0
+  let has_long_int = Sys.word_size == 64
   in
   let mk_ctx () =
     let config = Context.Config.create () in
@@ -11,13 +11,13 @@ let checkoverflow _ =
   let ctx = mk_ctx () in
   let int_typ = Type.int () in
   let var = Term.new_uninterpreted int_typ in
-  let value = Term.Arith.power (Term.Int.of_int 2) (if has_long_in then 62 else 30) in
+  let value = Term.Arith.power (Term.Int.of_int 2) (if has_long_int then 62 else 30) in
   let formula = Term.Arith.eq var value in
   Context.assert_formula ctx formula;
   let _status = Context.check ctx in
   let model = Context.get_model ctx in
 
   try
-    let _ = Model.get_int model var in OUnit2.assert_failure ""
+    let _ = Model.get_int model var in OUnit2.assert_failure "should have raised a Failure exception (_oy_binding_overflow_error() in misc.h)"
   with Failure _ ->
     ()
