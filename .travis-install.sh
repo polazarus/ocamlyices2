6b337c4cd5a43dc2cda284da31c011fb32afd191
Maël Valais <mael.valais@gmail.com>
2017-05-05 10:41:09 +0200

    appveyor/travis-ci: add osx+win build and upload sha256 on release
    Note that on travis-ci, the upload takes place on the 4.02.0 build.
    Also, we must 'make uninstall' on appveyor and travis-ci because we
    cache the ~/.opam folder.
    
    For release uploads, I use the host name.

--- .travis-install.sh
+++ .travis-install.sh
@@ -1,22 +1,22 @@
-DEBIAN_PACKAGES="ocaml-nox ocaml-native-compilers opam libgmp-dev"
-
-if [ -z "$NO_GPERF" ]
-then
-DEBIAN_PACKAGES="$DEBIAN_PACKAGES gperf"
-fi
-
-case "$OCAML_VERSION" in
-3|3.12|3.12.1) ppa=avsm/ocaml312+opam12 ;;
-4.00|4.00.1) ppa=avsm/ocaml40+opam12 ;;
-4.01|4.01.0) ppa=avsm/ocaml41+opam12 ;;
-4|4.02|4.02.0) ppa=avsm/ocaml42+opam12 ;;
-*) echo Unknown OCaml version: $OCAML_VERSION; exit 1 ;;
+case "$TRAVIS_OS_NAME" in
+  linux)
+    case "$OCAML_VERSION" in
+      3|3.12|3.12.1) ppa=avsm/ocaml312+opam12 ;;
+      4.00|4.00.1) ppa=avsm/ocaml40+opam12 ;;
+      4.01|4.01.0) ppa=avsm/ocaml41+opam12 ;;
+      4|4.02|4.02.0) ppa=avsm/ocaml42+opam12 ;;
+      *) echo Unknown OCaml version: $OCAML_VERSION; exit 1 ;;
+    esac
+    sudo add-apt-repository -y ppa:$ppa
+    sudo apt-get update -qq
+    # if NOGPERF is not defined, ${NOGPERF:+gperf} will expand to nothing
+    sudo apt-get install -qq ocaml-nox ocaml-native-compilers opam libgmp-dev ${NOGPERF:+gperf}
+  ;;
+  osx)
+    brew update > /dev/null
+    brew install opam # 'gmp' and 'coreutils' are already installed on travis
+  ;;
 esac
-
-sudo add-apt-repository -y ppa:$ppa
-sudo apt-get update -qq
-# if NOGPERF is not defined, ${NOGPERF:+gperf} will expand to nothing
-sudo apt-get install -qq $DEBIAN_PACKAGES
 
 opam init -y
 eval `opam config env`
